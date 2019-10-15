80baa93df89636351575b1630d63805bd95ebe71
Aaron William Langham <awlangham@gmail.com>
2019-10-14 20:23:48 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+num = 1
+
+for i in range(200):
+  seat = i + 1
+  
+  if (seat == 127) | (seat == 128):
+    continue
+  
+  if num % seat != 0:
+    num *= seat
+  print(num)
+  
+for i in range(200):
+  seat = i + 1
+  
+  if (seat2 == 127) | (seat2 == 128):
+    continue
+  print(str(seat2) + ": " + str(num % seat2))
