cdbc9d461974a614f6d9816a5d299110cf40bceb
Aaron William Langham <awlangham@gmail.com>
2019-10-19 13:45:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from math import gcd
+
 num = 1
 
-for i in range(200):
+for i in range(50):
   seat = i + 1
   
   if (seat == 127) or (seat == 128):
     continue
   
-  if num % seat != 0:
-    num *= seat
+  #if num % seat != 0:
+  #  num *= seat
+  num = int(num * seat/gcd(num, seat))
   print(num)
   
-for i in range(200):
+for i in range(50):
   seat = i + 1
   
   #if (seat2 == 127) || (seat2 == 128):
